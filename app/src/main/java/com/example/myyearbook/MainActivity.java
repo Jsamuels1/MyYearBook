14b59ea8a7f1b22a9c384543aca9428d562ae4cf
Josiah <Jsamuels@student.sjcny.edu>
2022-02-10 19:33:28 -0500

    commit 1

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+ Button btn= (Button) findViewById(R.id.btnevent);
+
+ btn.setOnClickListener(new View.OnClickListener(){
+@Override
+    public void onClick(View view){
+        final TextView textView  =(TextView) findViewById(R.id.textview);
+        textView.setText("Hello Hello Hello");
+
+        Intent i = new Intent(getApplicationContext(),GraduationPage.class);
+        startActivity(i);
+
+}
+
+});
+
+}
+
+
 }
